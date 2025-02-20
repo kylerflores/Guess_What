6d5d4685f84863d0760b5afd23e547043b6a4301
Kyler Flores <kyleryf@student.ubc.ca>
2025-02-20 15:42:37 -0800

    Set up file structure

--- src/main/board.py
+++
@@ -1,6 +0,0 @@
-"""This class represents the playing board, keeping track of the active frames and the players chosen frame"""
-
-class Board:
-    """docstring"""
-    def __init__(self, name):
-        self.name = name
---
+++ src/main/model/board.py
@@ -0,0 +1,10 @@
+"""
+This class represents the playing board that keeps track of:
+    - the active frames
+    - the players chosen frame
+"""
+
+class Board:
+    """docstring"""
+    def __init__(self, name):
+        self.name = name
