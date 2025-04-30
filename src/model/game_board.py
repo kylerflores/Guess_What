b48ec2e8ef12665f15f7de3ecb1347c13ede8495
Kyler Flores <kyleryf@student.ubc.ca>
2025-04-30 13:36:17 -0700

    Implemented GameBoard class and removed methods to adhere to the Uniform Access Principle

--- src/model/game_board.py
+++ src/model/game_board.py
@@ -19,19 +19,28 @@ class GameBoard:
         Args:
             character_dict (str : character): dictionary of characters
         """
+        self.chosen = None
+        self.character_dict = character_dict.copy()
+        self.num_unflipped = len(character_dict)
 
     def chose_round_character(self, chosen):
         """
         Args:
             chosen (character): The character that the player chooses for the current round
         """
+        self.chosen = chosen
 
     def reset_board(self):
         """
         resets the game boards by:
             - unflipping all characters
+            - reseting the num_flipped
             - reseting the chosen character
         """
+        for i in self.character_dict:
+            self.character_dict[i].flipped = False
+        self.num_unflipped = len(self.character_dict)
+        self.chosen = None
 
     def flip_character(self, character_id):
         """
@@ -40,6 +49,12 @@ class GameBoard:
         Args:
             character_id (str) : ID of the character we want to flip
         """
+        curr_character = self.character_dict[character_id]
+        if curr_character.flipped:
+            self.num_unflipped += 1
+        else:
+            self.num_unflipped -= 1
+        curr_character.flip()
 
     def is_flipped(self, character_id):
         """
@@ -47,14 +62,16 @@ class GameBoard:
 
         Args:
             character_id (str) : ID of the character we want to check status
+        
+        Returns:
+            bool: the flip status of the input character
         """
-
-    def num_unflipped(self):
-        """
-        returns the number of inflipped characters on the board
-        """
+        return self.character_dict[character_id].flipped
 
     def print_board_state(self):
         """
         prints the current state of the game board in the console 
         """
+        for i in self.character_dict:
+            if self.character_dict[i].flipped:
+                print(f"x{i}x")
